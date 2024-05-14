701a30035b97e5428162acb3f0f48dc1892dfc68
Вероника Бархатова <veronica.barkhatova@yandex.ru>
2024-05-14 15:43:58 +0400

    check workflow 2

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -7,9 +7,9 @@ load_dotenv()
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = os.getenv('SECRET_KEY', 'keys')
-
 DEBUG = os.getenv('DEBUG', 'False')
+
+SECRET_KEY = os.getenv('SECRET_KEY', 'keys')
 
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'localhost').split(',')
 
