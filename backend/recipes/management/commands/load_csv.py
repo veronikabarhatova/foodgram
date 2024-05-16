184e016c1350b609759b656cfab3987c45a48334
Вероника Бархатова <veronica.barkhatova@yandex.ru>
2024-05-16 18:59:22 +0400

    check workflow 6

--- backend/recipes/management/commands/load_csv.py
+++ backend/recipes/management/commands/load_csv.py
@@ -18,6 +18,12 @@ class Command(BaseCommand):
         ) as f:
             csv_reader = csv.reader(f, delimiter=',')
             for name, measurement_unit in csv_reader:
-                Ingredient.objects.get_or_create(
+                ingredient, created = Ingredient.objects.get_or_create(
                     name=name,
                     measurement_unit=measurement_unit)
+                if not created:
+                    self.stdout.write(self.style.WARNING(
+                        f'Ингредиент "{name}" уже существует.'))
+                else:
+                    self.stdout.write(self.style.SUCCESS(
+                        f'Ингредиент "{name}" успешно создан.'))
